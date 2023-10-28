5df63f91533e9613936549373626a32dbf811030
zhzhuang-zju <m17799853869@163.com>
2023-10-28 16:18:19 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-#
+#!/bin/bash
+
+comp=$1
+
+export VERSION="latest"
+export REGISTRY="docker.io/karmada"
+make image-${comp} GOOS="linux" --directory=.
+kind load docker-image docker.io/karmada/${comp}:latest --name karmada-host
+
+export KUBECONFIG=~/.kube/karmada.config
+kubectl --context karmada-host  rollout restart  deployment/${comp} -n karmada-system
